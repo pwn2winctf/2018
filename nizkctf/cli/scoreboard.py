d134185073b96470169ddf4a9d04881ec25f8164
Paulo Matias <paulo.matias@usp.br>
2017-01-15 00:55:08 -0200

    PEP8 styling

--- nizkctf/cli/scoreboard.py
+++ nizkctf/cli/scoreboard.py
@@ -6,6 +6,7 @@ import operator
 import tempfile
 import subprocess
 
+
 def rank(f):
     '''
     Compute ranking given a submissions file.
@@ -36,7 +37,8 @@ def rank(f):
             submissions[team] = [subm]
 
     return (sorted(scores.items(), key=operator.itemgetter(1),
-            reverse = True), submissions)
+            reverse=True), submissions)
+
 
 def pprint(ranking, top=0):
     '''
@@ -48,12 +50,13 @@ def pprint(ranking, top=0):
 
     '''
 
-    if top == 0: top = len(ranking)
+    if top == 0:
+        top = len(ranking)
     team_max_len = 10
     for team, score in ranking[0:top]:
         if len(team) > team_max_len:
             team_max_len = len(team)
-    team_max_len += 1 # add breathing space
+    team_max_len += 1  # add breathing space
 
     fmt = ' %-7s| %-' + str(team_max_len) + 's| %-8s'
     # fix for utf8 crown
@@ -62,19 +65,20 @@ def pprint(ranking, top=0):
 
     print('')
     print(sep)
-    print(fmt%('Pos', 'Team', 'Score'))
+    print(fmt % ('Pos', 'Team', 'Score'))
     print(sep)
 
     for idx, (team, score) in enumerate(ranking[0:top]):
         pos = str(idx+1)
         if idx == 0:
             pos += u' \U0001F451 '
-            print(fmtfst%(pos, team, str(score)))
+            print(fmtfst % (pos, team, str(score)))
         else:
-            print(fmt%(pos, team, str(score)))
+            print(fmt % (pos, team, str(score)))
 
     print(sep)
     print('')
+
 
 def plot(ranking, submissions, top=3):
     '''
@@ -90,18 +94,18 @@ def plot(ranking, submissions, top=3):
     # generate temporary files with data points
     fnames = []
     for team, _ in ranking[0:top]:
-        f = tempfile.NamedTemporaryFile(suffix='-%s.dat'%(team),
-            prefix='nizkctf-', delete=True)
+        f = tempfile.NamedTemporaryFile(suffix='-%s.dat' % team,
+                                        prefix='nizkctf-', delete=True)
         partial = 0
         for subm in submissions[team]:
             partial += subm['points']
-            f.write('%s, %d\n'%(subm['time'], partial))
+            f.write('%s, %d\n' % (subm['time'], partial))
         f.flush()
         fnames.append((team, f))
 
     # generate gnuplot file
     f = tempfile.NamedTemporaryFile(suffix='.gp',
-        prefix='nizkctf-', delete=True)
+                                    prefix='nizkctf-', delete=True)
     f.write('set terminal dumb 120 30\n')
     f.write('set xdata time\n')
     f.write('set datafile sep \',\'\n')
@@ -109,22 +113,19 @@ def plot(ranking, submissions, top=3):
     f.write('set style data steps\n')
     f.write('plot ')
     fmt = '\'%s\' using 1:2 title \'%s\''
-    f.write(fmt%(fnames[0][1].name, fnames[0][0]))
+    f.write(fmt % (fnames[0][1].name, fnames[0][0]))
     for team, ft in fnames[1:]:
         f.write(', ')
-        f.write(fmt%(ft.name, team))
+        f.write(fmt % (ft.name, team))
     f.flush()
 
     # plot in terminal
     p = subprocess.Popen(['gnuplot', f.name],
-        stderr = sys.stderr,
-        stdout = sys.stdout)
+                         stderr=sys.stderr,
+                         stdout=sys.stdout)
     p.wait()
 
     # close/delete files
     f.close()
     for nm, f in fnames:
         f.close()
-
-
-
