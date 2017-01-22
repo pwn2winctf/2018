e0c393e8bdca6c33d4f17efa7674dd3d5df7df27
Thiago Cardoso <thiagoncc@gmail.com>
2017-01-22 21:09:52 -0200

    fix: Base64 to avoid problems with salt

--- nizkctf/cli/challenges.py
+++ nizkctf/cli/challenges.py
@@ -23,18 +23,14 @@ def generate_keypair(flag, salt):
     return pysodium.crypto_sign_seed_keypair(chall_seed)
 
 def check_flag_key(flag_pk, salt, flag):
-    print flag, salt
     calc_pk, _ = generate_keypair(flag, salt)
-    print base64.b64encode(flag_pk)
-    print base64.b64encode(calc_pk)
     return flag_pk == calc_pk
 
 def check_flag(chall_stream, chall_id, flag):
-    log.info('checking if %s flag == %s'%(chall_id, flag))
     challs = json.load(chall_stream)
     for chall in challs:
         if chall['id'] == chall_id:
             return check_flag_key(base64.b64decode(chall['pk']),
-                    chall['salt'], flag)
+                    base64.b64decode(chall['salt']), flag)
     raise ChallError('Invalid challenge id: %s'%(chall_id))
 
