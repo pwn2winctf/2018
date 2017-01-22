382ce8cb0bc8967a1d4f4b5b7d8281a9aa73c0bd
Paulo Matias <paulo.matias@usp.br>
2017-01-22 19:53:02 -0200

    Implement same accessing pattern as Settings

--- nizkctf/localsettings.py
+++ nizkctf/localsettings.py
@@ -5,31 +5,23 @@ import json
 import threading
 
 
+thisdir = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(thisdir, '..', 'local-settings.json')
+
+
 class DefaultLocalSettings(object):
+    __lock__ = threading.Lock()
+
     def __init__(self):
-        self.data = None
-        self.lock = threading.Lock()
-        self._load()
+        if os.path.exists(path):
+            with open(path) as f:
+                self.__dict__.update(json.load(f))
 
-    def __setitem__(self, k, v):
-        self.data[k] = v
-        with self.lock:
-            self._store()
+    def __setattr__(self, k, v):
+        self.__dict__[k] = v
+        with self.__lock__:
+            with open(path, 'w') as f:
+                json.dump(self.__dict__, f)
 
-    def __getitem__(self, k):
-        return self.data[k]
-
-    def _load(self):
-        tdir = os.path.dirname(os.path.realpath(__file__))
-        try:
-            with open(os.path.join(tdir, '..', 'local-settings.json')) as f:
-                self.data = json.load(f)
-        except IOError:
-            self.data = {}
-
-    def _store(self):
-        tdir = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(tdir, '..', 'local-settings.json'), 'w') as f:
-            json.dump(self.data, f)
 
 LocalSettings = DefaultLocalSettings()
