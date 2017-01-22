78304f5974d5fe9d671a3d43919ed644baaac9da
Paulo Matias <paulo.matias@usp.br>
2017-01-22 20:22:35 -0200

    Initial skel for Team class

--- nizkctf/team.py
+++ nizkctf/team.py
@@ -2,6 +2,18 @@
 
 import os
 import json
+import pysodium
+from binascii import hexlify, unhexlify
 
 
-pass
+class Team(object):
+    def __init__(self, team_name=None, team_id=None):
+        if team_name:
+            team_id = name_to_id(team_name)
+        if team_id:
+            self.team_id = team_id
+        raise ValueError('Supply team_name or team_id')
+
+    @staticmethod
+    def name_to_id(name):
+        return hexlify(pysodium.crypto_hash_sha256(name))
