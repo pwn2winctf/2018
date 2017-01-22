39c7610b38ada480d4639df88c872b34920787c0
Thiago Cardoso <thiagoncc@gmail.com>
2017-01-22 15:50:56 -0200

    Team key generation

--- nizkctf/cli/team.py
+++ nizkctf/cli/team.py
@@ -4,36 +4,43 @@ import hashlib
 import os
 import json
 import codecs
+import pysodium
+import base64
 from . import log
 
 PLACEHOLDER_SUB_DIR='submissions/'
 
-def hashf(s):
-    '''placeholder. remove when hash is defined'''
-    return hashlib.sha1(s).hexdigest()
-
-def write_team_config(team_name, priv_key, public_key):
+def write_team_config(team_name, crypt_pk, crypt_sk, sign_pk, sign_sk):
     log.info('overriding team_key.json')
     with codecs.open('team_key.json', 'w', 'utf8') as f:
         f.write(json.dumps({
             'name': team_name,
-            'private_key': priv_key,
-            'public_key': public_key,
+            'crypt_pk': base64.b64encode(crypt_pk),
+            'crypt_sk': base64.b64encode(crypt_sk),
+            'sign_pk': base64.b64encode(sign_pk),
+            'sign_sk': base64.b64encode(sign_sk),
         }))
     log.success('new team key ready, share it with your team')
 
-def register(team_name, public_key):
+def register(team_name):
     log.info('registering new team: %s'%(team_name))
-    team_hash = hashf(team_name)
+    team_hash = hashlib.sha256(team_name).hexdigest()
     team_dir = os.path.join(PLACEHOLDER_SUB_DIR, team_hash)
 
     if os.path.exists(team_dir):
         log.fail('team is already registered')
         return False
 
+    log.info('generating crypt keypair')
+    crypt_pk, crypt_sk = pysodium.crypto_box_keypair()
+
+    log.info('generating signature keypair')
+    sign_pk, sign_sk = pysodium.crypto_sign_keypair()
+
     team_data = {
         'name': team_name,
-        'public_key': public_key,
+        'crypt_pk': base64.b64encode(crypt_pk),
+        'sign_pk': base64.b64encode(sign_pk),
     }
 
     os.mkdir(team_dir)
@@ -41,6 +48,8 @@ def register(team_name, public_key):
         f.write(json.dumps(team_data))
 
     log.success('team %s successfully added'%(team_name))
+
+    write_team_config(team_name, crypt_pk, crypt_sk, sign_pk, sign_sk)
 
     return True
 
