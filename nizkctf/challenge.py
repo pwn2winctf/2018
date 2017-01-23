5ae6c5ffb06715c8eb5c69cc94007a1148736338
Paulo Matias <paulo.matias@usp.br>
2017-01-23 17:45:39 -0200

    Add proof creation and chall keypair derivation

--- nizkctf/challenge.py
+++ nizkctf/challenge.py
@@ -1,9 +1,29 @@
 # -*- encoding: utf-8 -*-
 
 from __future__ import unicode_literals
+from base64 import b64decode
+import pysodium
+from .settings import Settings
 
 
 class Challenge(object):
     def __init__(self, chall_id):
         """ stub """
         self.pk = b''
+        self.salt = b''
+
+
+def derive_keypair(salt, flag):
+    flag = flag.encode('utf-8')
+    assert isinstance(salt, bytes)
+    assert isinstance(flag, bytes)
+    assert len(salt) == pysodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES
+
+    chall_seed = pysodium.crypto_pwhash_scryptsalsa208sha256(
+        pysodium.crypto_sign_SEEDBYTES,
+        flag,
+        salt,
+        Settings.scrypt_ops_limit,
+        Settings.scrypt_mem_limit)
+
+    return pysodium.crypto_sign_seed_keypair(chall_seed)
--- nizkctf/proof.py
+++ nizkctf/proof.py
@@ -5,9 +5,16 @@ import os
 import pysodium
 from base64 import b64encode, b64decode
 from .challenge import Challenge
+from .team import Team
+from .cli.teamsecrets import TeamSecrets
 
 
 def proof_open(team, proof):
+    proof = proof.encode('utf-8')
+
+    assert isinstance(team, Team)
+    assert isinstance(proof, bytes)
+
     proof = b64decode(proof)
 
     claimed_chall = proof[2*64:].decode('utf-8')
@@ -23,3 +30,16 @@ def proof_open(team, proof):
 
     return chall_id
 
+
+def proof_create(chall_id, chall_sk):
+    chall_id = chall_id.encode('utf-8')
+
+    assert isinstance(chall_id, bytes)
+    assert isinstance(chall_sk, bytes)
+
+    team_sk = TeamSecrets['sign_sk']
+
+    membership_proof = pysodium.crypto_sign(chall_id, team_sk)
+    proof = pysodium.crypto_sign(membership_proof, chall_sk)
+
+    return b64encode(proof).decode('utf-8')
