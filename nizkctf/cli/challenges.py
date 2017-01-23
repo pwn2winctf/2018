238de4f6c04502c0a88fc4cd4cb08fa54de545d6
Thiago Cardoso <thiagoncc@gmail.com>
2017-01-23 20:51:50 -0200

    Updating to last commit

--- nizkctf/cli/challenges.py
+++ nizkctf/cli/challenges.py
@@ -2,10 +2,12 @@
 
 from __future__ import unicode_literals
 import os
+import codecs
 import json
 import pysodium
 import base64
 from . import log
+from ..challenge import Challenge, derive_keypair
 
 
 OPSLIMIT = 33554432
@@ -15,27 +17,14 @@ MEMLIMIT = 1073741824
 class ChallError(Exception):
     pass
 
-
-def generate_keypair(flag, salt):
-    assert len(salt) == pysodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES
-    chall_seed = pysodium.crypto_pwhash_scryptsalsa208sha256(
-        pysodium.crypto_sign_SEEDBYTES,
-        flag,
-        salt,
-        OPSLIMIT,
-        MEMLIMIT)
-
-    return pysodium.crypto_sign_seed_keypair(chall_seed)
-
 def check_flag_key(flag_pk, salt, flag):
-    calc_pk, _ = generate_keypair(flag, salt)
+    calc_pk, _ = derive_keypair(salt, flag)
     return flag_pk == calc_pk
 
-def check_flag(chall_stream, chall_id, flag):
-    challs = json.load(chall_stream)
-    for chall in challs:
-        if chall['id'] == chall_id:
-            return check_flag_key(base64.b64decode(chall['pk']),
-                    base64.b64decode(chall['salt']), flag)
+def check_flag(chall_id, flag):
+    chall = Challenge(chall_id)
+    chall_data = json.load(codecs.open(chall.path(), 'r', 'utf8'))
+    return check_flag_key(base64.b64decode(chall_data['pk']),
+                    base64.b64decode(chall_data['salt']), flag)
     raise ChallError('Invalid challenge id: %s'%(chall_id))
 
