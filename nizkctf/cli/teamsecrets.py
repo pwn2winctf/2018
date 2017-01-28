cc36fe06de2ea74a4c6a2e6193b852cd6c5cef88
Paulo Matias <paulo.matias@usp.br>
2017-01-27 23:17:51 -0200

    Backup team-secrets.json if overriden

--- nizkctf/cli/teamsecrets.py
+++ nizkctf/cli/teamsecrets.py
@@ -3,6 +3,8 @@
 from __future__ import unicode_literals, division, print_function,\
                        absolute_import
 import os
+import shutil
+import time
 from . import log
 from ..serializable import SerializableDict
 
@@ -24,7 +26,11 @@ TeamSecrets = DefaultTeamSecrets()
 
 
 def write_team_secrets(team_id, crypt_sk, sign_sk):
-    log.info('overriding %s' % TEAMSECRETS_FILE)
+    if TeamSecrets.exists():
+        log.info('overriding %s' % TEAMSECRETS_FILE)
+
+        backup = TeamSecrets.path() + time.strftime('.%Y-%m-%d-%H-%M-%S')
+        shutil.copy(TeamSecrets.path(), backup)
 
     TeamSecrets['id'] = team_id
     TeamSecrets['crypt_sk'] = crypt_sk
